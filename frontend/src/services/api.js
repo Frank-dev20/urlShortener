70c1cd2da414caaf756258b280784c5b4f6e91a6
Frank-dev20 <frank.joseph@sabi.am>
2025-10-14 13:27:46 +0100

    updated the api.js file

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -7,21 +7,6 @@ const api = axios.create({
     }
 });
 
-api.interceptors.request.use(
-    (config)=>{
-        const token = localStorage.getItem('token');
-        if(token){
-            config.headers.Authorization = `Bearer ${token}`;
-
-        }
-        return config
-        },
-        (error)=>{
-        return Promise.reject(error)
-    }
-);
-
-
 // Request interceptor - Add JWT token to every request
 api.interceptors.request.use(
   (config) => {
